c7668628fc66a3d785429cb901383bb15f1a08bc
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-10-06 12:12:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function insertionSortReverse(array) {
+    for(var i = (arr.length - 1); i > 0; i--)
+        {
+            var val = arr[i];
+            var j;
+            for(j = 1; j < i && arr[j-1] < val; j++)
+                {
+                    arr[j-1] = arr[j];
+                }
+            arr[j] = val;
+        }
     return array;
 }
